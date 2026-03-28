9cf2cea0e08411a521e1a9660f4c8dd13b59b2d0
haroonpuzhakkal07-pixel <haroonpuzhakkal07@gmail.com>
2026-03-29 00:29:16 +0530

    User registration implemented with form and auto login

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
+from django.contrib.auth import login
+from django.http import HttpResponse
 
 # Create your views here.
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = RegisterForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
+def home_view(request):
+    return HttpResponse("Home Page")
