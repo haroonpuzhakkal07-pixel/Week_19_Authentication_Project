a79f9fa36ecdc083d590b33399b0d221af4d7ba1
haroonpuzhakkal07-pixel <haroonpuzhakkal07@gmail.com>
2026-03-30 16:54:19 +0530

    Login System implemented using Django AuthenticationForm

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import register_view, home_view
+from .views import register_view, home_view, login_view
 
 urlpatterns = [
-    path('register/', register_view , name='register'),
     path('', home_view, name='home'),
+    path('register/', register_view , name='register'),
+    path('login/', login_view, name='login')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from .forms import RegisterForm
-from django.contrib.auth import login
+from .forms import RegisterForm, LoginForm
+from django.contrib.auth import login, authenticate
 from django.http import HttpResponse
 
 # Create your views here.
@@ -18,5 +18,21 @@ def register_view(request):
         form = RegisterForm()
     return render(request, 'accounts/register.html', {'form': form})
 
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+    else:
+        form = LoginForm()
+    return render(request, 'accounts/login.html', {'form': form})
 def home_view(request):
-    return HttpResponse("Home Page")
+    if request.user.is_authenticated:
+        return HttpResponse(f"Welcome {request.user.username}")
+    else:
+        return HttpResponse('Welcome Guest')
